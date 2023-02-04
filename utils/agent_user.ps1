230fbb86795d15d4663773f0b536b66482ed960d
dannhh <hongdancuibap@gmail.com>
2023-02-04 14:55:23 +0700

    Update agent user file

--- utils/agent_user.ps1
+++ utils/agent_user.ps1
@@ -1,4 +1,6 @@
-$user_obj = Get-ChildItem Env:\USERNAME
-$user = $user_obj.Value
+user_obj=Get-ChildItem Env:\USERNAME
+user=$user_obj.Value
+pwsh -Command "Write-Host "Agent username is '$user'""
+pwsh -Command "Write-Host "##vso[task.setvariable variable=agent_username;]$user""
 Write-Host "Agent username is '$user'"
 Write-Host "##vso[task.setvariable variable=agent_username;]$user"
