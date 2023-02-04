b1031a40b24ff04e84f5cfe7e8b73ebbc431df9e
dannhh <hongdancuibap@gmail.com>
2023-02-04 13:14:34 +0700

    update install requirement

--- package_requirement/install_requirements.sh
+++ package_requirement/install_requirements.sh
@@ -5,6 +5,7 @@ pip install h5py
 pip install typing-extensions
 pip install wheel
 
+pip install --upgrade cython
 pip install --upgrade azure-cli
 pip install --upgrade azureml-sdk
 pip install -r requirements.txt
