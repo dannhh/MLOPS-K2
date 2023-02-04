34546cb94505ddab805486d126fe3d6c24313469
dannhh <hongdancuibap@gmail.com>
2023-02-04 15:12:44 +0700

    Restore agent_user file

--- utils/agent_user.ps1
+++ utils/agent_user.ps1
@@ -1,6 +1,3 @@
-user_obj=Get-ChildItem Env:\USERNAME
-user=$user_obj.Value
-pwsh -Command "Write-Host "Agent username is '$user'""
-pwsh -Command "Write-Host "##vso[task.setvariable variable=agent_username;]$user""
+$user = (Get-ChildItem Env:\USERNAME).Value
 Write-Host "Agent username is '$user'"
 Write-Host "##vso[task.setvariable variable=agent_username;]$user"
