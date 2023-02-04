cc15bea12445416231f7ec3a29ec8245c45501fe
dannhh <hongdancuibap@gmail.com>
2023-02-04 12:44:24 +0700

    update install requirement

--- package_requirement/install_requirements.sh
+++ package_requirement/install_requirements.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 python --version
+
+pip install h5py
+pip install typing-extensions
+pip install wheel
+
 pip install --upgrade azure-cli
 pip install --upgrade azureml-sdk
 pip install -r requirements.txt
