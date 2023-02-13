b9a7314781070a57b9c981f6be6089b42e1c929b
dannhh <hongdancuibap@gmail.com>
2023-02-13 21:25:38 +0700

    Update requirement file

--- package_requirement/install_requirements.sh
+++ package_requirement/install_requirements.sh
@@ -6,6 +6,17 @@ pip install typing-extensions
 pip install wheel
 
 pip install --upgrade cython
+
+# The Azure command-line interface (Azure CLI) is a set of commands used to 
+# create and manage Azure resources. 
 pip install --upgrade azure-cli
+
+# The Azure Machine Learning SDK is used to build and run machine learning 
+# workflows upon the Azure Machine Learning service, interact with the service 
+# in any Python environment, including Jupyter Notebooks or Python IDE.
 pip install --upgrade azureml-sdk
+
+pip install azure.keyvault
+pip install azureml.pipeline
+
 pip install -r requirements.txt
