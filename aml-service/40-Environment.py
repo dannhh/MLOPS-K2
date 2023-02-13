7d56faa305e62ecdfe28bfd59fc61f7f6759ffe2
dannhh <hongdancuibap@gmail.com>
2023-02-14 01:49:27 +0700

    Fix:\u00ef\u00bb\u00bfname

--- aml-service/40-Environment.py
+++ aml-service/40-Environment.py
@@ -15,7 +15,9 @@ def register_environment(env_config):
     # Get workspace
     ws = Workspace.from_config(auth=cli_auth)
     # Get environment configuration from yml file
+    print("Check/Start")
     env_name = env_config['name']
+    print("Check/End")
     env_file = env_config['file_path']
     env = Environment.from_conda_specification(env_name, env_file)
     # Register the enviromnment
